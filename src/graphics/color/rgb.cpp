75b72de995be8240065225325a55889b8cc4036d
Felipe Endlich <endlichfelipe@gmail.com>
2022-01-24 01:36:00 -0300

    rgba color

---
+++ src/graphics/color/rgb.cpp
@@ -0,0 +1,101 @@
+#include "rgb.hpp"
+
+using namespace graphics::color;
+
+#pragma region Constructors and Destructors
+RGBA::RGBA(unsigned int r, unsigned int g, unsigned int b)
+{
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = 255;
+}
+
+RGBA::RGBA(unsigned int r, unsigned int g, unsigned int b, unsigned int a)
+{
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = a;
+}
+
+RGBA::RGBA(const RGBA &other)
+{
+    this->r = other.r;
+    this->g = other.g;
+    this->b = other.b;
+    this->a = other.a;
+}
+
+RGBA::RGBA(const RGBA &&other)
+{
+    this->r = other.r;
+    this->g = other.g;
+    this->b = other.b;
+    this->a = other.a;
+}
+
+#pragma endregion // Constructors and Destructors
+
+#pragma region Operators
+RGBA &RGBA::operator=(const RGBA &other)
+{
+    this->r = other.r;
+    this->g = other.g;
+    this->b = other.b;
+    this->a = other.a;
+
+    return *this;
+}
+
+RGBA &RGBA::operator=(const RGBA &&other)
+{
+    this->r = other.r;
+    this->g = other.g;
+    this->b = other.b;
+    this->a = other.a;
+
+    return *this;
+}
+#pragma endregion // Operators
+
+#pragma region Getters and Setters
+unsigned int RGBA::getR() const
+{
+    return this->r;
+}
+
+unsigned int RGBA::getG() const
+{
+    return this->g;
+}
+unsigned int RGBA::getB() const
+{
+    return this->b;
+}
+
+unsigned int RGBA::getA() const
+{
+    return this->a;
+}
+
+void RGBA::setR(unsigned int r)
+{
+    this->r = r;
+}
+
+void RGBA::setG(unsigned int g)
+{
+    this->g = g;
+}
+
+void RGBA::setB(unsigned int b)
+{
+    this->b = b;
+}
+
+void RGBA::setA(unsigned int a)
+{
+    this->a = a;
+}
+#pragma endregion // Getters and Setters
---
+++ src/graphics/color/rgb.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace graphics
+{
+    namespace color
+    {
+        class RGBA
+        {
+        public:
+            RGBA(unsigned int r, unsigned int g, unsigned int b);
+            RGBA(unsigned int r, unsigned int g, unsigned int b, unsigned int a);
+            RGBA(const RGBA &other);
+            RGBA(const RGBA &&other);
+            
+            RGBA &operator=(const RGBA &other);
+            RGBA &operator=(const RGBA &&other);
+
+            unsigned int getR() const;
+            unsigned int getG() const;
+            unsigned int getB() const;
+            unsigned int getA() const;
+
+            void setR(unsigned int r);
+            void setG(unsigned int g);
+            void setB(unsigned int b);
+            void setA(unsigned int a);
+
+        private:
+            unsigned int r;
+            unsigned int g;
+            unsigned int b;
+            unsigned int a;
+        };
+    }
+}
