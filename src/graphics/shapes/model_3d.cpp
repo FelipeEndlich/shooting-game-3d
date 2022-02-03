7c8b45ec644ca7a51fc78fef04d441e5ddafebf2
Felipe Endlich <endlichfelipe@gmail.com>
2022-02-03 19:24:26 -0300

    model 3D
    Missing transformation method

---
+++ src/graphics/shapes/model_3d.cpp
@@ -0,0 +1,183 @@
+#include "model_3d.hpp"
+
+#include <stdexcept>
+
+#include <GL/glut.h>
+
+#include "./model.hpp"
+#include "./../color/rgba.hpp"
+#include "./../../math/matrix.hpp"
+#include "./../../math/vector.hpp"
+
+using ::graphics::color::RGBA;
+using ::graphics::shapes::Model;
+using ::graphics::shapes::Model3D;
+using ::math::Matrix;
+using ::math::Vector;
+
+Model3D::Model3D()
+    : Model(), angle_(0)
+{
+}
+
+Model3D::Model3D(const math::Matrix &matrix)
+    : Model(matrix), angle_(0)
+{
+    ValidateMatrix(matrix);
+}
+
+Model3D::Model3D(const math::Matrix &matrix, const graphics::color::RGBA &color)
+    : Model(matrix, color), angle_(0)
+{
+    ValidateMatrix(matrix);
+}
+
+Model3D::Model3D(const Model3D &other)
+    : Model(other), angle_(other.angle_)
+{
+}
+
+Model3D::Model3D(const Model3D &&other)
+    : Model(other), angle_(other.angle_)
+{
+}
+
+void Model3D::Translate(double dx, double dy, double dz)
+{
+    Vector translation(3);
+    translation[0] = dx;
+    translation[1] = dy;
+    translation[2] = dz;
+    Translate(translation);
+}
+
+void Model3D::Translate(const math::Vector &vector)
+{
+    for (int i = 0; i < points_.get_rows(); i++)
+        points_[i] += vector;
+}
+
+void Model3D::Scale(double x, double y, double z, double sx, double sy, double sz)
+{
+    Transform(x, y, z, sx, sy, sz, 0, 0, 0);
+}
+
+void Model3D::Scale(const math::Vector &center, double sx, double sy, double sz)
+{
+    Transform(center, sx, sy, sz, 0, 0, 0);
+}
+
+void Model3D::Scale(double x, double y, double z, const math::Vector &scale_vector)
+{
+    Transform(x, y, z, scale_vector, 0, 0, 0);
+}
+
+void Model3D::Scale(const math::Vector &center, const math::Vector &scale_vector)
+{
+    Transform(center, scale_vector, 0, 0, 0);
+}
+
+void Model3D::Rotate(double x, double y, double z, double phi, double psi, double theta)
+{
+    Transform(x, y, z, 1, 1, 1, phi, psi, theta);
+}
+
+void Model3D::Rotate(double x, double y, double z, math::Vector &radians)
+{
+    Transform(x, y, z, 1, 1, 1, radians);
+}
+
+void Model3D::Rotate(const math::Vector &center, double phi, double psi, double theta)
+{
+    Transform(center, 1, 1, 1, phi, psi, theta);
+}
+
+void Model3D::Rotate(const math::Vector &center, math::Vector &radians)
+{
+    Transform(center, 1, 1, 1, radians);
+}
+
+void Model3D::Transform(const math::Matrix &matrix)
+{
+    for (int i = 0; i < points_.get_rows(); i++)
+    {
+        Vector point = Vector::Fill(4, 1);
+        point[0] = points_[i][0];
+        point[1] = points_[i][1];
+        point[2] = points_[i][2];
+
+        point = matrix * point;
+
+        points_[i][0] = point[0];
+        points_[i][1] = point[1];
+        points_[i][2] = point[2];
+    }
+}
+
+void Model3D::Transform(double x, double y, double z, double sx, double sy, double sz, double phi, double psi, double theta)
+{
+    // TODO: Implement transformation
+}
+
+void Model3D::Transform(const math::Vector &center, double sx, double sy, double sz, double phi, double psi, double theta)
+{
+    Transform(center[0], center[1], center[2], sx, sy, sz, phi, psi, theta);
+}
+
+void Model3D::Transform(double x, double y, double z, const math::Vector &scale, double phi, double psi, double theta)
+{
+    Transform(x, y, z, scale[0], scale[1], scale[2], phi, psi, theta);
+}
+
+void Model3D::Transform(double x, double y, double z, double sx, double sy, double sz, const math::Vector &radians)
+{
+    Transform(x, y, z, sx, sy, sz, radians[0], radians[1], radians[2]);
+}
+
+void Model3D::Transform(const math::Vector &center, const math::Vector &scale, double phi, double psi, double theta)
+{
+    Transform(center[0], center[1], center[2], scale[0], scale[1], scale[2], phi, psi, theta);
+}
+
+void Model3D::Transform(const math::Vector &center, double sx, double sy, double sz, const math::Vector &radians)
+{
+    Transform(center[0], center[1], center[2], sx, sy, sz, radians[0], radians[1], radians[2]);
+}
+
+void Model3D::Transform(double x, double y, double z, const math::Vector &scale, const math::Vector &radians)
+{
+    Transform(x, y, z, scale[0], scale[1], scale[2], radians[0], radians[1], radians[2]);
+}
+
+void Model3D::Transform(const math::Vector &center, const math::Vector &scale, const math::Vector &radians)
+{
+    Transform(center[0], center[1], center[2], scale[0], scale[1], scale[2], radians[0], radians[1], radians[2]);
+}
+
+void Model3D::Draw()
+{
+    double red = color_.get_red() / 255.0;
+    double green = color_.get_green() / 255.0;
+    double blue = color_.get_blue() / 255.0;
+    double alpha = color_.get_alpha() / 255.0;
+
+    glColor4d(red, green, blue, alpha);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < points_.get_rows(); i++)
+        glVertex3d(points_[i][0], points_[i][1], points_[i][2]);
+
+    glEnd();
+}
+
+Vector Model3D::get_angle() const
+{
+    return angle_;
+}
+
+void Model3D::ValidateMatrix(const math::Matrix &matrix)
+{
+    if (matrix.get_columns() != 3)
+    {
+        throw std::invalid_argument("The matrix must have 3 columns.");
+    }
+}
---
+++ src/graphics/shapes/model_3d.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "./model.hpp"
+#include "./../color/rgba.hpp"
+#include "./../../math/matrix.hpp"
+#include "./../../math/vector.hpp"
+
+namespace graphics::shapes
+{
+    class Model3D : public Model
+    {
+    public:
+        Model3D();
+        Model3D(const math::Matrix &matrix);
+        Model3D(const math::Matrix &matrix, const graphics::color::RGBA &color);
+        Model3D(const Model3D &other);
+        Model3D(const Model3D &&other);
+        virtual ~Model3D() = default;
+
+        virtual void Translate(double dx, double dy, double dz);
+        virtual void Translate(const math::Vector &vector);
+
+        virtual void Scale(double x, double y, double z, double sx, double sy, double sz);
+        virtual void Scale(const math::Vector &center, double sx, double sy, double sz);
+        virtual void Scale(double x, double y, double z, const math::Vector &scale_vector);
+        virtual void Scale(const math::Vector &center, const math::Vector &scale_vector);
+
+        virtual void Rotate(double x, double y, double z, double phi, double psi, double theta);
+        virtual void Rotate(double x, double y, double z, math::Vector &radians);
+        virtual void Rotate(const math::Vector &center, double phi, double psi, double theta);
+        virtual void Rotate(const math::Vector &center, math::Vector &radians);
+
+        virtual void Transform(const math::Matrix &matrix);
+        virtual void Transform(double x, double y, double z, double sx, double sy, double sz, double phi, double psi, double theta);
+        virtual void Transform(const math::Vector &center, double sx, double sy, double sz, double phi, double psi, double theta);
+        virtual void Transform(double x, double y, double z, const math::Vector &scale, double phi, double psi, double theta);
+        virtual void Transform(double x, double y, double z, double sx, double sy, double sz, const math::Vector &radians);
+        virtual void Transform(const math::Vector &center, const math::Vector &scale, double phi, double psi, double theta);
+        virtual void Transform(const math::Vector &center, double sx, double sy, double sz, const math::Vector &radians);
+        virtual void Transform(double x, double y, double z, const math::Vector &scale, const math::Vector &radians);
+        virtual void Transform(const math::Vector &center, const math::Vector &scale, const math::Vector &radians);
+
+        virtual void Draw();
+
+        virtual math::Vector get_angle() const;
+        virtual math::Vector get_center_position() const = 0;
+
+    protected:
+        math::Vector angle_;
+
+    private:
+        void ValidateMatrix(const math::Matrix &matrix);
+    };
+}
