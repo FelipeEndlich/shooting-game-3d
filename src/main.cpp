f01c07bba6fdcaa85b4055464d0ea6b1080445ec
Felipe Endlich <endlichfelipe@gmail.com>
2022-02-05 13:08:44 -0300

    usefull rotation object funct

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,31 @@ using namespace std;
 #include "./game/game.hpp"
 #include "./graphics/color/rgba.hpp"
 #include "./graphics/color/rgba_factory.hpp"
-//#include "./graphics/shapes/cuboid.hpp"
+#include "./graphics/shapes/sphere.hpp"
 #include <cmath>
 #include "./math/matrix.hpp"
+#include "./math/vector.hpp"
 
 using ::graphics::color::ColorOption;
 using ::graphics::color::RGBA;
 using ::graphics::color::RGBAFactory;
-//using ::graphics::shapes::Cuboid;
+using ::graphics::shapes::Sphere;
+using ::math::Vector;
+// using ::graphics::shapes::Cuboid;
+
+void displayFunc(void)
+{
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glRotatef(0.1, 1, 0, 0); // Rotate x axis
+    glRotatef(0.1, 0, 1, 0); // Rotate y axis
+    glRotatef(0.1, 0, 0, 1); // Rotate z axis
+
+    Sphere sphere(30, Vector::Zero(3), RGBAFactory::get_color(ColorOption::kWhite));
+    sphere.Draw();
+
+    glutSwapBuffers();
+}
 
 int main(int argc, char **argv)
 {
@@ -39,103 +56,17 @@ int main(int argc, char **argv)
     glutInitWindowPosition(100, 100);
     glutCreateWindow("2D GAME");
 
+    glutDisplayFunc(displayFunc);
+
+    glutIdleFunc([]()
+                 { glutPostRedisplay(); });
+
     /* selecionar cor de fundo (preto) */
     RGBA scren_color = RGBAFactory::get_color(ColorOption::kBlack);
     glClearColor(scren_color.get_red(), scren_color.get_green(), scren_color.get_blue(), scren_color.get_alpha());
 
     glLoadIdentity();
     glOrtho(-50, 50, -50, 50, -50, 50);
-
-    // Insert shapes here
-
-    /* Teste de Cuboid do Jorge */
-    /*
-    //Cuboid cuboidA(1, 1, 1);
-    math::Matrix m(3, 1);
-    float width = 20, height = 20, depth = 20;
-
-    // General coordinates
-    float xc, yc, zc, rotation;
-    xc = m[0][0];
-    yc = m[0][1];
-    zc = m[0][2];
-
-    rotation = 60;
-    
-    glRotatef(rotation, 1, 0, 0);
-    glRotatef(rotation, 0, 1, 0);
-    glBegin(GL_LINES);
-        // Front lines
-        float x00, x01, y00, y01, z0, z1;
-        x00 = xc - width / 2;
-        x01 = xc + width / 2;
-        y00 = yc + height / 2;
-        y01 = yc - height / 2;
-        z0 = zc - depth / 2;
-
-        glColor3f(1, 0, 0);
-
-        // Front superior line
-            glVertex3f(x00, y00, z0);
-            glVertex3f(x01, y00, z0);
-
-        // Front right line
-            glVertex3f(x01, y00, z0);
-            glVertex3f(x01, y01, z0);
-        
-        // Front inferior line
-            glVertex3f(x01, y01, z0);
-            glVertex3f(x00, y01, z0);
-
-        // Front left line
-            glVertex3f(x00, y01, z0);
-            glVertex3f(x00, y00, z0);
-
-        // Back lines
-        z1 = zc + depth / 2;
-
-        // Back superior line
-            glVertex3f(x00, y00, z1);
-            glVertex3f(x01, y00, z1);
-
-        // Back right line
-            glVertex3f(x01, y00, z1);
-            glVertex3f(x01, y01, z1);
-        
-        // Back inferior line
-            glVertex3f(x01, y01, z1);
-            glVertex3f(x00, y01, z1);
-
-        // Back left line
-            glVertex3f(x00, y01, z1);
-            glVertex3f(x00, y00, z1);
-
-        // Connection lines
-
-        // Left superior line
-            glVertex3f(x00, y00, z0);
-            glVertex3f(x00, y00, z1);
-
-        // Right superior line
-            glVertex3f(x01, y00, z0);
-            glVertex3f(x01, y00, z1);
-
-        // Left inferior line
-            glVertex3f(x00, y01, z0);
-            glVertex3f(x00, y01, z1);
-
-        // Right inferior line
-            glVertex3f(x01, y01, z0);
-            glVertex3f(x01, y01, z1);
-
-    glEnd();
-    glRotatef(-rotation, 0, 1, 0);
-    glRotatef(-rotation, 1, 0, 0);
-
-    glFlush();
-    glutSwapBuffers();
-    */
-   /* Fim do teste de Cuboid do Jorge */
 
     glutMainLoop();
 
