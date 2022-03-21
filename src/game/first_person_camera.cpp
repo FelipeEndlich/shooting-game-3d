dc4b506dfc317d19bf7487108b2876ab637ab374
Jorge Uliana <ulianamjjorge@gmail.com>
2022-03-21 13:51:32 -0300

    Small corrections at the angle limiting

--- src/game/first_person_camera.cpp
+++ src/game/first_person_camera.cpp
@@ -3,6 +3,7 @@
 #include "../math/vector.hpp"
 #include <GL/glut.h>
 #include <cmath>
+#include <iostream>
 
 using math::Vector;
 using shoot_and_jump::Camera;
@@ -16,7 +17,8 @@ FirstPersonCamera::FirstPersonCamera(const math::Vector &character_position)
     Vector center = Vector::ThreeDimPoint(character_position[0] + 1, character_position[1], character_position[2]);
 
     center_ = Vector::ThreeDimPoint(center[0], center[1], center[2]);
-    up_ = Vector::ThreeDimPoint(0, -1, 0);
+    up_ = Vector::ThreeDimPoint(0, 0, 1).CrossProduct(center_ - eye_);
+    up_[1] = -up_[1];
 }
 
 void FirstPersonCamera::Move(const math::Vector &movement)
@@ -30,9 +32,9 @@ void FirstPersonCamera::Rotate(const math::Vector &angles)
     double rho_squared, tan_theta, cos_phi;
     double rho, theta, phi;
     double x, y, z;
-    x = center_[0];
-    y = center_[1];
-    z = center_[2];
+    x = center_[0] - eye_[0];
+    y = center_[1] - eye_[1];
+    z = center_[2] - eye_[2];
 
     rho_squared = pow(x, 2) + pow(y, 2) + pow(z, 2);
     tan_theta = y / x;
@@ -40,9 +42,19 @@ void FirstPersonCamera::Rotate(const math::Vector &angles)
 
     rho = sqrt(rho_squared);
     theta = atan(tan_theta) - angles[0];
-    phi = acos(cos_phi) + asin(angles[1]);
+    phi = acos(cos_phi) + angles[1];
 
-    center_[0] = rho * sin(phi) * cos(theta);
-    center_[1] = rho * sin(phi) * sin(theta);
-    center_[2] = rho * cos(phi);
+    double base_phi, base_theta;
+    base_phi = phi / M_PI - 0.5;
+    base_theta = theta / M_PI;
+
+    if (!(-0.5 < base_phi && base_phi < 0.5) || !(-0.2 < base_theta && base_theta < 0.2))
+        return;
+
+    center_[0] = rho * sin(phi) * cos(theta) + eye_[0];
+    center_[1] = rho * sin(phi) * sin(theta) + eye_[1];
+    center_[2] = rho * cos(phi) + eye_[2];
+
+    up_ = Vector::ThreeDimPoint(0, 0, 1).CrossProduct(center_ - eye_);
+    up_[1] = -up_[1];
 }
